309a50ce97900a8a28acd77c4cee7173be7d5809
Sushil1204 <sushilspundkar@gmail.com>
2022-12-23 15:55:30 +0530

    update index.js
    components added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
+import Card from "../components/Card";
+import Navigation from "../components/Navigation";
+import PostForm from "../components/PostForm";
 
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Welcome</h1>
+      <div className="flex mt-4 container mx-auto">
+        <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 py-6 gap-8">
+          <div className="">
+            <Navigation/>
+          </div>
+          <div className="col-span-2 ">
+            <div>
+              <PostForm/>
+            </div>
+            <div>Post</div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
