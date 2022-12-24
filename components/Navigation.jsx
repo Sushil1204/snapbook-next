402d575651935cdb5381bd655772625722b2fb83
Sushil1204 <sushilspundkar@gmail.com>
2022-12-25 01:32:59 +0530

    update navigation.jsx
    active nonactive element modification done

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
 import Card from "./Card";
+
 const Navigation = () => {
+  const activeElement = 'flex py-3 bg-socialBlue -mx-10 px-10 rounded-md shadow-md shadow-gray-300';
+  const nonActiveElement = 'flex py-2 my-2 hover:bg-blue-500 hover:bg-opacity-20 -mx-4 px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300';
   return (
     <Card>
       <div className="px-4">
         <h2 className="text-gray-400 mb-3">Navigation</h2>
-        <a href="" className="flex py-3">
+        <a href="" className={activeElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -23,7 +25,7 @@ const Navigation = () => {
           </svg>
           Home
         </a>
-        <a href="" className="flex py-3">
+        <a href="" className={nonActiveElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -40,7 +42,7 @@ const Navigation = () => {
           </svg>
           Friends
         </a>
-        <a href="" className="flex gap-3 py-3">
+        <a href="" className={nonActiveElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -57,7 +59,7 @@ const Navigation = () => {
           </svg>
           Saved Post
         </a>
-        <a href="" className="flex py-3">
+        <a href="" className={nonActiveElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -74,7 +76,7 @@ const Navigation = () => {
           </svg>
           Notifications
         </a>
-        <a href="" className="flex py-3">
+        <a href="" className={nonActiveElement}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
